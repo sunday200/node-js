b436476bca255def19f1cc844040279776d569d9
Sandy Arfian <sandyarfian.2019@gmail.com>
2022-11-07 11:17:54 +0700

    ubah dikit

--- controllers/Input1Controller.js
+++ controllers/Input1Controller.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const m$todo = require('../modules/input1.modules')
 const response = require('../helpers/response')
 
-const TodoController = Router()
+const SensorController = Router()
 
 
 /**
@@ -11,17 +11,17 @@ const TodoController = Router()
  * @param {string} description 
  */
 
-TodoController.get('/', async (req, res, next) => {
+SensorController.get('/', async (req, res, next) => {
      const add = await m$todo.listSensor()
 
      response.sendResponse(res, add)
 })
 
-TodoController.post("/", async (req, res, next) => {
+SensorController.post("/", async (req, res, next) => {
      const add = await m$todo.addSensor(req.body)
 
      response.sendResponse(res, add)
 })
 
 
-module.exports = TodoController
+module.exports = SensorController
