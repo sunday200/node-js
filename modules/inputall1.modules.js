b09783a75bf6f790cb9d54aaa0036dae27ee9dd1
Sandy Arfian <sandyarfian.2019@gmail.com>
2022-11-08 10:26:09 +0700

    add input all

---
+++ modules/inputall1.modules.js
@@ -0,0 +1,98 @@
+// Helper database yang dibuat
+const mysql = require("../helpers/database")
+// Validation input
+const Joi = require('joi')
+
+class _blog {
+
+     listSensor  = async () => {
+          try {
+               const list = await mysql.query(
+                    'SELECT * FROM tangkap_sensor_all',
+                    []
+               )
+     
+               return {
+                    status : true,
+                    data: list
+               }
+          }  catch (error) {
+               console.error("list data sensor Error", error)
+               
+               return {
+                    status : false,
+                    error
+               }
+          }}
+
+     addSensor = async (body) => {
+          try {
+               const schema = Joi.object({
+                    sensor1_adc: Joi.number().required(),
+                    sensor2_adc: Joi.number().required(),
+                    sensor3_adc: Joi.number().required(),
+                    sensor4_adc: Joi.number().required(),
+                    sensor5_adc: Joi.number().required(),
+                    sensor6_adc: Joi.number().required(),
+                    sensor1_res: Joi.number().required(),
+                    sensor2_res: Joi.number().required(),
+                    sensor3_res: Joi.number().required(),
+                    sensor4_res: Joi.number().required(),
+                    sensor5_res: Joi.number().required(),
+                    sensor6_res: Joi.number().required(),
+                    sensor1_ch4: Joi.number().required(),
+                    sensor1_co: Joi.number().required(),
+                    sensor1_h2: Joi.number().required(),
+                    sensor2_co: Joi.number().required(),
+                    sensor2_ch4: Joi.number().required(),
+                    sensor3_ch4: Joi.number().required(),
+                    sensor3_co: Joi.number().required(),
+                    sensor3_h2: Joi.number().required(),
+                    sensor4_nh3: Joi.number().required(),
+                    sensor4_h2: Joi.number().required(),
+                    sensor5_h2: Joi.number().required(),
+                    sensor5_ch4: Joi.number().required(),
+                    sensor6_co: Joi.number().required(),
+                    sensor6_h2: Joi.number().required(),
+                    sensor6_c4h10: Joi.number().required()
+               })
+
+               const validation = schema.validate(body)
+
+               if(validation.error) {
+                    const errorDetails = validation.error.details.map(detail => detail.message)
+                    
+                    return {
+                         status: false,
+                         code: 422,
+                         error: errorDetails.join(", ")
+                    }
+               }
+          
+               const add = await mysql.query(
+                    "INSERT INTO tangkap_sensor_all(sensor1_adc, sensor2_adc, sensor3_adc, sensor4_adc, sensor5_adc, sensor6_adc, sensor1_res, sensor2_res, sensor3_res, sensor4_res, sensor5_res, sensor6_res, sensor1_ch4, sensor1_co, sensor1_h2, sensor2_co, sensor2_ch4, sensor3_ch4, sensor3_co, sensor3_h2, sensor4_nh3, sensor4_h2, sensor5_h2, sensor5_ch4, sensor6_co, sensor6_h2, sensor6_c4h10) VALUES (?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+
+                    [body.sensor1_adc, body.sensor2_adc, body.sensor3_adc, body.sensor4_adc, body.sensor5_adc, body.sensor6_adc, body.sensor1_res, body.sensor2_res, body.sensor2_res, body.sensor3_res, body.sensor4_res, body.sensor5_res, body.sensor6_res, body.sensor1_ch4, body.sensor1_co, body.sensor1_h2, body.sensor2_co, body.sensor2_ch4, body.sensor3_ch4, body.sensor3_co, body.sensor3_h2, body.sensor4_nh3, body.sensor4_h2, body.sensor5_h2, body.sensor5_ch4, body.sensor6_co, body.sensor6_h2, body.sensor6_c4h10]
+               )
+               return{
+                    status : true,
+                    data: add
+               }
+
+          } catch (error) {
+               console.error("addnilai sensor module Error: ", error)
+
+               return {
+                    status: false,
+                    error
+               }
+
+          }
+     }
+
+
+}
+
+module.exports = new _blog()
+     
+
--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
 const alat1Controller = require('./controllers/Input1Controller')
 const alat2Controller = require('./controllers/Input2Controller')
 const BuzzerController = require('./controllers/BuzzerController')
+const InputAll1Controller = require('./controllers/InputAll1Controller')
 
 
 // Define url API in Here
 const _routes = [
      ['/alat-1', alat1Controller],
      ['/alat-2', alat2Controller],
-     ['/buzzer-1', BuzzerController]
+     ['/buzzer-1', BuzzerController],
+     ['/alat-1-all', InputAll1Controller]
 
 ]
 
