5d181219649398f0e04d2be39e4f713f7006436a
Sandy Arfian <sandyarfian.2019@gmail.com>
2022-11-17 10:10:48 +0700

    tambahan api untuj ambil dataset

--- routes.js
+++ routes.js
@@ -2,17 +2,21 @@ const alat1Controller = require('./controllers/Input1Controller')
 const alat2Controller = require('./controllers/Input2Controller')
 const BuzzerController = require('./controllers/BuzzerController')
 const InputAll1Controller = require('./controllers/InputAll1Controller')
+const InputAll1RealController = require('./controllers/InputAll1RealController')
+const InputAll2RealController = require('./controllers/InputAll2RealController')
 const KlasifikasiController = require('./controllers/KlasifikasiController')
 
 
 
 // Define url API in Here
 const _routes = [
-     ['/alat-1', alat1Controller],
-     ['/alat-2', alat2Controller],
-     ['/buzzer-1', BuzzerController],
-     ['/alat-1-all', InputAll1Controller],
-     ['/hasil-1', KlasifikasiController]
+     ['/alat-1', alat1Controller], // untuk alat 1
+     ['/alat-2', alat2Controller], // untuk alat 2
+     ['/buzzer-1', BuzzerController], // untuk buzzer
+     ['/alat-1-all', InputAll1Controller], // untuk alat-1 semua nilai
+     ['/hasil-1', KlasifikasiController], // untuk coba coba
+     ['/alat-1-fix', InputAll1RealController], // untuk alat 1 adc resistansi dan ppm (sementara dipakai untuk ambil dataset)
+     ['/alat-2-fix', InputAll2RealController] // untuk alat 2 adc resistansi dan ppm (sementara dipakai untuk ambil dataser)
 
 ]
 
