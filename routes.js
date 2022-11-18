c7b2b5c4a313a408acc7ba6980b16d48b1daeb00
Sandy Arfian <sandyarfian.2019@gmail.com>
2022-11-18 14:36:04 +0700

    tambahkan alat 2

--- routes.js
+++ routes.js
@@ -5,6 +5,11 @@ const InputAll1Controller = require('./controllers/InputAll1Controller')
 const Input1adcController = require('./controllers/Input1adcController')
 const Input1resController = require('./controllers/Input1resController')
 const Input1ppmController = require('./controllers/Input1ppmController')
+
+const Input2adcController = require('./controllers/Input2adcController')
+const Input2resController = require('./controllers/Input2resController')
+const Input2ppmController = require('./controllers/Input2ppmController')
+
 const InputAll2RealController = require('./controllers/InputAll2RealController')
 const KlasifikasiController = require('./controllers/KlasifikasiController')
 
@@ -17,9 +22,14 @@ const _routes = [
      ['/buzzer-1', BuzzerController], // untuk buzzer
      ['/alat-1-all', InputAll1Controller], // untuk alat-1 semua nilai
      ['/hasil-1', KlasifikasiController], // untuk coba coba
-     ['/alat-1-adc', Input1adcController], // untuk alat 1 adc resistansi dan ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-1-res', Input1resController], // untuk alat 1 adc resistansi dan ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ppm', Input1ppmController], // untuk alat 1 adc resistansi dan ppm (sementara dipakai untuk ambil dataset)
+     
+     ['/alat-1-adc', Input1adcController], // untuk alat 1 adc (sementara dipakai untuk ambil dataset)
+     ['/alat-1-res', Input1resController], // untuk alat 1 resistansi (sementara dipakai untuk ambil dataset)
+     ['/alat-1-ppm', Input1ppmController], // untuk alat 1 ppm (sementara dipakai untuk ambil dataset)
+     ['/alat-2-adc', Input2adcController], // untuk alat 2 adc(sementara dipakai untuk ambil dataset)
+     ['/alat-2-res', Input2resController], // untuk alat 2 resistansi(sementara dipakai untuk ambil dataset)
+     ['/alat-2-ppm', Input2ppmController], // untuk alat 2 ppm (sementara dipakai untuk ambil dataset)
+
      ['/alat-2-fix', InputAll2RealController] // untuk alat 2 adc resistansi dan ppm (sementara dipakai untuk ambil dataser)
 
 ]
