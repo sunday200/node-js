7ea9a2aa0262195c1e0df9b5b7bda66df2cf5366
Sandy Arfian <sandyarfian.2019@gmail.com>
2022-12-27 06:32:38 +0700

    apus yang tidak perlu

--- routes.js
+++ routes.js
@@ -1,23 +1,20 @@
-const alat1Controller = require('./controllers/Input1Controller')
-const alat2Controller = require('./controllers/Input2Controller')
 const Buzzer1Controller = require('./controllers/Buzzer1Controller')
 const Buzzer2Controller = require('./controllers/Buzzer2Controller')
-const InputAll1Controller = require('./controllers/InputAll1Controller')
+
 const Input1adcController = require('./controllers/Input1adcController')
 const Input1resController = require('./controllers/Input1resController')
 const Input1ppmController = require('./controllers/Input1ppmController')
-const Input1ppmHASILController = require('./controllers/Input1ppmHASILController')
 const Input1roController = require('./controllers/Input1roController')
-
-const Input1ALKOHOLadcController = require('./controllers/Input1ALKOHOLadcController')
-const Input1ALKOHOLresController = require('./controllers/Input1ALKOHOLresController')
-const Input1ALKOHOLppmController = require('./controllers/Input1ALKOHOLppmController')
-const Input1ALKOHOLroController = require('./controllers/Input1ALKOHOLroController')
 
 const Input2adcController = require('./controllers/Input2adcController')
 const Input2resController = require('./controllers/Input2resController')
 const Input2ppmController = require('./controllers/Input2ppmController')
 const Input2roController = require('./controllers/Input2roController')
+
+const Input1ALKOHOLadcController = require('./controllers/Input1ALKOHOLadcController')
+const Input1ALKOHOLresController = require('./controllers/Input1ALKOHOLresController')
+const Input1ALKOHOLppmController = require('./controllers/Input1ALKOHOLppmController')
+const Input1ALKOHOLroController = require('./controllers/Input1ALKOHOLroController')
 
 const Input2ALKOHOLadcController = require('./controllers/Input2ALKOHOLadcController')
 const Input2ALKOHOLresController = require('./controllers/Input2ALKOHOLresController')
@@ -27,51 +24,35 @@ const Input2ALKOHOLroController = require('./controllers/Input2ALKOHOLroControll
 const InputCar1Controller = require('./controllers/InputCar1Controller')
 const InputCar2Controller = require('./controllers/InputCar2Controller')
 
-const InputAll2RealController = require('./controllers/InputAll2RealController')
-const KlasifikasiController = require('./controllers/KlasifikasiController')
-
-
 
 // Define url API in Here
 const _routes = [
-     ['/alat-1', alat1Controller], // untuk alat 1
-     ['/alat-2', alat2Controller], // untuk alat 2
      ['/buzzer-1', Buzzer1Controller], // untuk buzzer alat 1
      ['/buzzer-2', Buzzer2Controller], // untuk buzzer alat 2
-     ['/alat-1-all', InputAll1Controller], // untuk alat-1 semua nilai
-     ['/hasil-1', KlasifikasiController], // untuk coba coba
-     
-     ['/alat-1-adc', Input1adcController], // untuk alat 1 adc (sementara dipakai untuk ambil dataset)
-     ['/alat-1-res', Input1resController], // untuk alat 1 resistansi (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ppm', Input1ppmController], // untuk alat 1 ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ro', Input1roController], // untuk alat 1 ro (sementara dipakai untuk ambil dataset)
 
-     ['/1-alkohol-adc', Input1ALKOHOLadcController], // ini sampai baris ke 54 untuk ambil dataset alkohol
-     ['/1-alkohol-res', Input1ALKOHOLresController],
-     ['/1-alkohol-ppm', Input1ALKOHOLppmController], 
-     ['/1-alkohol-ro', Input1ALKOHOLroController], 
+     ['/alat-1-adc', Input1adcController], // untuk alat 1 adc 
+     ['/alat-1-res', Input1resController], // untuk alat 1 resistansi 
+     ['/alat-1-ppm', Input1ppmController], // untuk alat 1 ppm 
+     ['/alat-1-ro', Input1roController], // untuk alat 1 ro 
 
-     ['/2-alkohol-adc', Input2ALKOHOLadcController], 
-     ['/2-alkohol-res', Input2ALKOHOLresController],
-     ['/2-alkohol-ppm', Input2ALKOHOLppmController], 
-     ['/2-alkohol-ro', Input2ALKOHOLroController], 
+     ['/alat-2-adc', Input2adcController], // untuk alat 2 adc
+     ['/alat-2-res', Input2resController], // untuk alat 2 resistansi
+     ['/alat-2-ppm', Input2ppmController], // untuk alat 2 ppm
+     ['/alat-2-ro', Input2roController], // untuk alat 2 ro 
 
-     ['/alat-1-adc', Input1adcController], // untuk alat 1 adc (sementara dipakai untuk ambil dataset)
-     ['/alat-1-res', Input1resController], // untuk alat 1 resistansi (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ppm', Input1ppmController], // untuk alat 1 ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ppm-hasil', Input1ppmHASILController], // untuk alat 1 ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-1-ro', Input1roController], // untuk alat 1 ro (sementara dipakai untuk ambil dataset)
+     ['/1-alkohol-adc', Input1ALKOHOLadcController], // untuk ambil dataset
+     ['/1-alkohol-res', Input1ALKOHOLresController], // untuk ambil dataset
+     ['/1-alkohol-ppm', Input1ALKOHOLppmController], // untuk ambil dataset
+     ['/1-alkohol-ro', Input1ALKOHOLroController], // untuk ambil dataset
 
-     ['/alat-2-adc', Input2adcController], // untuk alat 2 adc(sementara dipakai untuk ambil dataset)
-     ['/alat-2-res', Input2resController], // untuk alat 2 resistansi(sementara dipakai untuk ambil dataset)
-     ['/alat-2-ppm', Input2ppmController], // untuk alat 2 ppm (sementara dipakai untuk ambil dataset)
-     ['/alat-2-ro', Input2roController], // untuk alat 2 ro (sementara dipakai untuk ambil dataset)
+     ['/2-alkohol-adc', Input2ALKOHOLadcController], // untuk ambil dataset
+     ['/2-alkohol-res', Input2ALKOHOLresController], // untuk ambil dataset
+     ['/2-alkohol-ppm', Input2ALKOHOLppmController], // untuk ambil dataset
+     ['/2-alkohol-ro', Input2ALKOHOLroController], // untuk ambil dataset
 
-     ['/alat-carCO', InputCar1Controller], // CAR CO
-     ['/alat-carO2', InputCar2Controller], // CAR 02
 
-     ['/alat-2-fix', InputAll2RealController] // untuk alat 2 adc resistansi dan ppm (sementara dipakai untuk ambil dataser)
-
+     ['/alat-carCO', InputCar1Controller], // CAR CO untuk pengambilan dataser Enose For Car
+     ['/alat-carO2', InputCar2Controller], // CAR 02 untuk pengambilan dataset Enose For Car
 ]
 
 // https://localhost:5001/tes
@@ -85,4 +66,3 @@ const routes = (app) => {
 
 module.exports = routes 
 
-// kalo class exports nya harus pake new
